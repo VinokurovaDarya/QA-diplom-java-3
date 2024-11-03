4a3a1457025ce6db97d0f57171015b6e6d1c164b
Darya Vinokurova <vinokurova.d97@mail.ru>
2024-11-03 17:41:36 +0400

    commit

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,2 +1,23 @@
-package org.example;public class User {
+package org.example;
+
+import java.util.Random;
+
+public class User {
+    private String email = "lunch" + new Random().nextInt(10000) + "@yandex.ru";
+    private String password = "evening" + new Random().nextInt(10000);
+    private String name = "sun" + new Random().nextInt(10000);
+
+    public String getRandomName() {
+        return this.name;
+    }
+
+    public  String getRandomEmail() {
+        return this.email;
+    }
+
+    public  String getRandomPassword() {
+        return this.password;
+    }
+
+
 }
